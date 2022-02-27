4cfe067017545c2725987886b42fd3dd364c792e
Reetik Patel <Reetik2000@yahoo.com>
2022-02-27 01:27:05 -0600

    🔧 Disabled image optimiation

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/hackathon/' : ''
+  assetPrefix: isProd ? '/hackathon/' : '',
+  images: {
+    loader: "custom",
+    // TODO: Use the new ACM ImgIX account for optimization
+  }
 }
 
 module.exports = nextConfig
