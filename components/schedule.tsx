1bce31956587d4b1bd1aa926ae0d0c7409cb216c
Reetik Patel <Reetik2000@yahoo.com>
2022-02-26 22:29:36 -0600

    🚧 Init event schedule section

---
+++ components/schedule.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Schedule.module.scss";
+
+const Schedule = () => {
+    return (
+        <div>Schedule</div>
+    )
+}
+
+export default Schedule;
---
+++ stories/components/schedule.stories.tsx
@@ -0,0 +1,9 @@
+import Schedule from "../../components/schedule";
+
+
+export default {
+  title: "Event Schedule",
+  component: Schedule,
+};
+
+export const EventSchedule = () => <Schedule />
