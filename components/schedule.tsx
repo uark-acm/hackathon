f2131556eaa826613eb205b78595e7675cf9c3c4
Reetik Patel <Reetik2000@yahoo.com>
2022-02-27 00:28:19 -0600

    ✨ Schedule section

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -1,8 +1,58 @@
+import { startDate, endDate, day1Schedule, day2Schedule } from "../EDITME";
 import styles from "../styles/Schedule.module.scss";
 
 const Schedule = () => {
+    const parseDate = (numericalDate: string) => {
+        const date = new Date(numericalDate);
+        const dateString = date.toLocaleDateString('en-us', {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric'
+        });
+
+        if(date.getDate() > 3 && date.getDate() < 12) {
+            return dateString + "th";
+        } else {
+            switch(date.getDate() % 10) {
+                case 1: return dateString + "st";
+                case 2: return dateString + "nd";
+                case 3: return dateString + "rd";
+                default: return dateString + "th";
+            }
+        }
+    }
+
+    const displaySchedule = (schedule: Array<{ time: string, text: string}>) => {
+        return (
+            <ul>
+                {
+                    schedule.map(entry => {
+                        return (
+                            <li>
+                                <span>{entry.time}</span> - {entry.text}
+                            </li>
+                        )
+                    })
+
+                }                
+            </ul>
+        );
+    }
+
     return (
-        <div>Schedule</div>
+        <div className={styles.schedule}>
+            <h2 className={styles.sectionTitle}>Event Schedule</h2>
+            <div className={styles.content}>
+                <div className={styles.day}>
+                    <h4>{parseDate(startDate)}</h4>
+                    <p>{displaySchedule(day1Schedule)}</p>
+                </div>
+                <div className={styles.day}>
+                    <h4>{parseDate(endDate)}</h4>
+                    <p>{displaySchedule(day2Schedule)}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
