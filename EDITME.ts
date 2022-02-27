aa66998687a43dd88784edd9327f235258a83ded
Reetik Patel <Reetik2000@yahoo.com>
2022-02-26 20:20:33 -0600

    🚧🔧📝 Added templating file

---
+++ EDITME.ts
@@ -0,0 +1,45 @@
+// Site templating configs
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   Content                                  */
+/* -------------------------------------------------------------------------- */
+/**
+ * Start Date of the competition
+ * Format: MM/DD/YYYY
+ */
+export const startDate = "04/01/2022";
+
+/**
+ * End Date of the competition
+ * Format: MM/DD/YYYY
+ */
+export const endDate = "04/02/2022";
+
+/**
+ * Day 1 and Day 2 Schedules
+ * UX Consideration: Maintain the spaces for consistency
+ *   For example...
+ *        12:00 AM
+ *         7:00 AM
+ *   There is a space before the 7 to align it with 12:00 AM above.
+ *   The site uses a monospaced font(Lato) for this to make it aligned but
+ *   you still have to add the space to actually align the content.
+ */
+export const day1Schedule = `
+ 5:30 PM - Opening Session
+ 8:00 PM - Dinner
+ 9:00 PM - Final Project Approvals
+`;
+
+export const day2Schedule = `
+12:00 AM - Midnight Snack
+ 7:00 AM - Breakfast
+ 4:00 PM - Closing Session
+`;
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 Site Config                                */
+/* -------------------------------------------------------------------------- */
+// TBD
