b253fca1a33e3adf80153939ba281f5853b0972c
Reetik Patel <Reetik2000@yahoo.com>
2022-02-27 20:40:07 -0600

    🚧 Init countdown bar

---
+++ components/countdown-bar.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/CountdownBar.module.scss';
+
+const CountdownBar = () => {
+    return (
+        <div>
+            Countdown Bar
+        </div>
+    );
+}
+
+export default CountdownBar;
---
+++ stories/components/countdown-bar.stories.tsx
@@ -0,0 +1,8 @@
+import CountdownBar from "../../components/countdown-bar";
+
+export default {
+  title: "Countdown",
+  component: CountdownBar,
+};
+
+export const Countdown = () => <CountdownBar />
