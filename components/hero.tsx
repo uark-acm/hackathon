8c56c46ac419489de83cc817fef3658209ae6f46
Reetik Patel <Reetik2000@yahoo.com>
2022-03-01 23:19:34 -0600

    🚧 Added season text logic

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,33 @@
-import { subtitle } from '../EDITME';
+import { startDate, subtitle } from '../EDITME';
 import styles from '../styles/Hero.module.scss';
 
 const Hero = () => {
+    const SeasonText = () => {
+        const date = new Date(startDate);
+        let text = "";
+        if(date.getMonth() <= 6) {
+            text = "Spring";
+        } else {
+            text = "Fall";
+        }
+        text += " " + date.getFullYear();
+
+        return (
+            <h2>
+                {
+                    Array.from(text).map(char => {
+                        return <div>{char}</div>
+                    })
+                }
+            </h2>
+        )
+    }
+    
+
     return (
         <div className={styles.hero}>
             <div className={styles.content}>
+                <SeasonText />
                 <h1>Hackathon</h1>
                 <h6>{subtitle}</h6>
             </div>
