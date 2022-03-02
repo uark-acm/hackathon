9a0cfa3eda5bb3925ceb164b3225f21aa1690820
reeeeeee-dev <reeeeeee-dev@users.noreply.github.com>
2022-03-02 08:03:57 +0000

    deploy: e35230a5b67af2253b38f3d18624ffaea21afaa1

---
+++ _next/static/8Z7gMTwVDJQJGb-lUQq_Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/91fb0cf0b6cad1e3.css","static/chunks/pages/index-42bb5d36c46a6477.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/wERdxkY1sCtBbi-Pkjv-n/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/907c020ed8d4a936.css","static/chunks/pages/index-5ba7ca5e5a22f81a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
