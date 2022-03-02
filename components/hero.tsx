7a660ff3e6cb6f9caf503721e84927f90cdb4a93
Reetik Patel <Reetik2000@yahoo.com>
2022-03-01 22:02:33 -0600

    🚧 Hero content block

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,13 @@
+import { subtitle } from '../EDITME';
 import styles from '../styles/Hero.module.scss';
 
 const Hero = () => {
     return (
         <div className={styles.hero}>
-            Hero
+            <div className={styles.content}>
+                <h1>Hackathon</h1>
+                <h6>{subtitle}</h6>
+            </div>
         </div>
     );
 }
