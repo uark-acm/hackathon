7db51f96dfc199dbf18418e694d313c83ac0f11f
Reetik Patel <Reetik2000@yahoo.com>
2022-03-07 00:54:10 -0600

    🚧 Init details section

---
+++ components/details.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/details.module.scss";
+
+const Details = () => {
+    return(
+        <p>WIP</p>
+    )
+}
+
+export default Details;
---
+++ stories/components/details.stories.tsx
@@ -0,0 +1,8 @@
+import Details from "../../components/details";
+
+export default {
+  title: "Details",
+  component: Details,
+};
+
+export const details = () => <Details />
