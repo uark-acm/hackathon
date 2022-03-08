3cdab15ec5bf31011bc42ff8e5cbc27809e8ab14
Reetik Patel <Reetik2000@yahoo.com>
2022-03-08 17:11:42 -0600

    🚧💄 Footer styles with image

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import styles from "../styles/Footer.module.scss";
+import Button from "./button";
 
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            Footer
+            <div className={styles.logo}>
+                <Image layout="fill" src="/img/jbhunt.svg" />
+            </div>
+            <p>24 hours to code, so grab a duck and join!</p>
+            <Button className={styles.button} variant="tertiary" text="Back to Top ▲" />
         </div>
     )
 }
