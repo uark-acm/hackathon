e5e4331f90ae7931ccb9fdf35eb8ec7ebe515df1
Reetik Patel <Reetik2000@yahoo.com>
2022-03-08 12:11:00 -0600

    💄 Added better size scaling to images

--- components/details.tsx
+++ components/details.tsx
@@ -8,13 +8,13 @@ const Details = () => {
         <div className={styles.details}>
             <div className={styles.images}>
                 <div className={styles.image1}>
-                    <Image height={256} width={320} src="https://unsplash.it/0?random=1" />
+                    <Image layout="fill" src="https://unsplash.it/0?random=1" />
                 </div>
                 <div className={styles.image2}>
-                    <Image height={352} width={464} src="https://unsplash.it/0?random=2" />
+                    <Image layout="fill" src="https://unsplash.it/0?random=2" />
                 </div>
                 <div className={styles.image3}>
-                    <Image height={352} width={416} src="https://unsplash.it/0/0?random=3" />
+                    <Image layout="fill" src="https://unsplash.it/0/0?random=3" />
                 </div>
             </div>
             <div className={styles.content}>
