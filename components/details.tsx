4d44ab53bcc1fddb92af52b544974b14565fbeb4
Reetik Patel <Reetik2000@yahoo.com>
2022-03-09 21:43:38 -0600

    🚧 Applied local images to details

--- components/details.tsx
+++ components/details.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
-import { detailsText, detailsTitle, registrationLink } from "../EDITME";
+import {
+    detailsText,
+    detailsTitle,
+    image1Name,
+    image2Name,
+    image3Name,
+    registrationLink
+} from "../EDITME";
 import styles from "../styles/Details.module.scss";
 import Button from "./button";
 
@@ -9,13 +16,13 @@ const Details = () => {
         <div className={styles.details}>
             <div className={styles.images}>
                 <div className={styles.image1}>
-                    <Image layout="fill" src="https://unsplash.it/0?random=1" />
+                    <Image layout="fill" src={`/img/${image1Name}`} />
                 </div>
                 <div className={styles.image2}>
-                    <Image layout="fill" src="https://unsplash.it/0?random=2" />
+                    <Image layout="fill" src={`/img/${image2Name}`} />
                 </div>
                 <div className={styles.image3}>
-                    <Image layout="fill" src="https://unsplash.it/0/0?random=3" />
+                    <Image layout="fill" src={`/img/${image3Name}`} />
                 </div>
             </div>
             <div className={styles.content}>
