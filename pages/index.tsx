73027d8e22ae426f4a04ad0500fb38efa01bcffb
Reetik Patel <Reetik2000@yahoo.com>
2022-03-09 21:08:24 -0600

    🚸 Switched Schedule and Details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Button from '../components/button'
 import CountdownBar from '../components/countdown-bar'
 import Details from '../components/details'
 import Footer from '../components/footer'
 import Hero from '../components/hero'
 import Schedule from '../components/schedule'
+import { registrationLink } from '../EDITME'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
@@ -21,8 +23,8 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <Hero />
         <CountdownBar />
-        <Details />
         <Schedule />
+        <Details />
       </main>
 
       <Footer />
